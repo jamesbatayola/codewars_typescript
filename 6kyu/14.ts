2b0249f872fd9b1d9f461b30bb7c2768ea87dc74
jamesbatayola <batayolajakejames@gmail.com>
2025-08-18 08:15:54 +0800

    6-16

--- 6kyu/14.ts
+++ 6kyu/14.ts
@@ -1,6 +1,5 @@
 export function countBits(n: number): number {
-  // Program Me
-  return n.toString(2).replace("0", "").length;
+  return n.toString(2).replace(/0/g, "").length;
 }
 
 console.log(countBits(1234));
---
+++ 6kyu/16.ts
@@ -0,0 +1,28 @@
+// Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case). The next words should be always capitalized.
+
+// Examples
+// "the-stealth-warrior" gets converted to "theStealthWarrior"
+
+// "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+
+// "The_Stealth-Warrior" gets converted to "TheStealthWarrior"
+
+export function toCamelCase(str: string): string {
+  const words: string[] = str.toLowerCase().split(/[-_]/g);
+
+  let res = words[0];
+
+  for (let i = 1; i < words.length; i++) {
+    res += words[i].replace(words[i][0], words[i][0].toUpperCase());
+  }
+
+  return res;
+}
+
+const t1 = "the-stealth-warrior";
+const t2 = "The_Stealth_Warrior";
+const t3 = "The_Stealth-Warrior";
+
+console.log(toCamelCase(t1));
+console.log(toCamelCase(t2));
+console.log(toCamelCase(t3));
